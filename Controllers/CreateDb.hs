aa4bb0c9c1e092ae8bd0a23ebc9a3a255259b28a
frankcash <cashc@acm.org>
2014-10-07 00:08:57 -0400

    (#1) This will create the DB or connect to it if it already exists
    Adds the file to do it: Controllers/CreateDb.hs
    Adds a readme so the data `dir` gets pushed
    Adds an update to app.cabal
    Adds a comment in Main.hs to explain the file
    In the future make sure to capitilize the beginning of all files that
    need to be imported

--- Controllers/CreateDb.hs
+++ Controllers/CreateDb.hs
@@ -6,11 +6,17 @@ import Database.HDBC
 import Database.HDBC.Sqlite3
 
 
-initDB :: String
-initDB = do
-  conn = connectSqlite3 "test1.db"
-  run conn "CREATE TABLE test (id INTEGER NOT NULL, desc VARCHAR(80))" []
-  run conn "INSERT INTO test (id) VALUES (0)" []
 
+initDB = do
+  conn <- connectSqlite3 databaseFilePath
+  run conn ("CREATE TABLE podcasts" ++
+               " ( id     INTEGER PRIMARY KEY ASC " ++
+               " , number INTEGER NOT NULL " ++
+               " , topics TEXT NOT NULL " ++
+               " , guests TEXT NOT NULL " ++
+               " , start_ INTEGER NOT NULL ) ") []
   commit conn
   disconnect conn
+  return ()
+
+databaseFilePath = "data/hacktober.db"
--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Applicative((<$>))
 import Controllers.Home (home, login, foo, foor)
+import Controllers.CreateDb (initDB)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Network.Wai.Middleware.Static (addBase, noDots, staticPolicy, (>->))
 import System.Environment (getEnv)
@@ -9,6 +10,7 @@ import Web.Scotty (middleware, scotty)
 
 main :: IO()
 main = do
+  initDB --creates DB or connects
   scotty 3000 $ do
     middleware $ staticPolicy (noDots >-> addBase "Static/images") -- for favicon.ico
     home >> login >> foo >> foor
