c4271a353ed416b1484b4206dd8a779407727707
frankcash <cashc@acm.org>
2014-10-08 23:20:43 -0400

    Changes create DB

--- Controllers/CreateDb.hs
+++ Controllers/CreateDb.hs
@@ -9,12 +9,9 @@ import Database.HDBC.Sqlite3
 
 initDB = do
   conn <- connectSqlite3 databaseFilePath
-  run conn ("CREATE TABLE podcasts" ++
-               " ( id     INTEGER PRIMARY KEY ASC " ++
-               " , number INTEGER NOT NULL " ++
-               " , topics TEXT NOT NULL " ++
-               " , guests TEXT NOT NULL " ++
-               " , start_ INTEGER NOT NULL ) ") []
+  run conn ("CREATE TABLE users" ++
+               " ( uuid INTEGER PRIMARY KEY ASC " ++
+               " , name TEXT NOT NULL ) ") []
   commit conn
   disconnect conn
   return ()
