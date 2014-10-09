d2afeb5a54ac166f09407b2b119d777727b82723
frankcash <cashc@acm.org>
2014-10-09 01:26:31 -0400

    (#1) working on db for createUser
    Build fails, do not pull yet.
    Applys a monad transform per
    http://stackoverflow.com/questions/25588883/scotty-post-type-wont-allow-effects
    Need to fix createUserDB function though, error:
    
    Controllers/Home.hs:50:6:
        No instance for
        (convertible-1.1.0.0:Data.Convertible.Base.Convertible
                               a0 SqlValue)
    			         arising from a use of `toSql'
    				     Possible fix:
    				           add an instance declaration
    					   for
    					         (convertible-1.1.0.0:Data.Convertible.Base.Convertible
    						 a0 SqlValue)
    						     In the expression:
    						     toSql
    						         In the
    							 expression:
    							 toSql $ userId
    							     In the
    							     third
    							     argument of
    							     `run',
    							     namely
    							           `[toSql
    								   $
    								   userId,
    								   toSql
    								   $
    								   name]'
    
    								   Controllers/Home.hs:50:22:
    								       No
    								       instance
    								       for
    								       (convertible-1.1.0.0:Data.Convertible.Base.Convertible
    								                              a1
    											      SqlValue)
    											            arising
    												    from
    												    a
    												    use
    												    of
    												    `toSql'
    												        Possible
    													fix:
    													      add
    													      an
    													      instance
    													      declaration
    													      for
    													            (convertible-1.1.0.0:Data.Convertible.Base.Convertible a1 SqlValue)
    														        In
    															the
    															expression:
    															toSql
    															    In
    															    the
    															    expression:
    															    toSql
    															    $
    															    name
    															        In
    																the
    																third
    																argument
    																of
    																`run',
    																namely
    																      `[toSql $ userId, toSql $ name]'
    																      '`

--- Controllers/Home.hs
+++ Controllers/Home.hs
@@ -12,6 +12,9 @@ import Web.Scotty (ScottyM, ActionM, get, html, param)
 import Data.Monoid (mconcat)
 import Views.Home (homeView)
 import Views.Foor (foorView)
+import Database.HDBC
+import Database.HDBC.Sqlite3
+import Control.Monad.Trans ( MonadIO(liftIO) )
 
 
 home :: ScottyM ()
@@ -25,14 +28,29 @@ foo = get "/foo" $ html "Hello, this is foo!"
 
 -- createUser :: ScottyM()
 -- createUser =
+-- createUser ::  Connection -> Redis a -> ActionM a -- correct, but not this use case
+-- createUser ::  ActionM a -> Connection -> IO() -- need to figure out
 
-createUser :: ScottyM()
+createUser ::  ScottyM()
 createUser = get "/create/user/:userId/:name" $ do
   name <- param "name"
   userId <- param "userId"
+  liftIO $ createUserDB name userId -- monad transform
   html $ mconcat ["<p>/create/user/" , userId , "/" , name ,"</p>"]
 
 
 
 foor :: ScottyM()
 foor = get "/404"  foorView
+
+createUserDB :: a1 -> a0 -> IO()
+createUserDB name userId = do
+  conn <- connectSqlite3 databaseFilePath
+  run conn "INSERT INTO users VALUES (? , ?)"
+    [toSql $ userId, toSql $ name]
+  commit conn
+  disconnect conn
+
+
+
+databaseFilePath = "data/hacktober.db"
