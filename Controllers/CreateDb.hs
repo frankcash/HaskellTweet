45d6818f8c7b36b88469d251836be50d25b7d92d
frankcash <cashc@acm.org>
2014-10-10 23:57:51 -0400

    (fix #6) table now persistent
    Adds a basic if/else statement so I don't have to recreate it every time

--- Controllers/CreateDb.hs
+++ Controllers/CreateDb.hs
@@ -6,12 +6,16 @@ import Database.HDBC
 import Database.HDBC.Sqlite3
 
 
-
 initDB = do
   conn <- connectSqlite3 databaseFilePath
-  run conn ("CREATE TABLE users" ++
-               " ( uuid INTEGER PRIMARY KEY ASC " ++
-               " , name TEXT NOT NULL ) ") []
+  f <- getTables conn
+  if length f > 0
+    then
+      run conn ("SELECT * FROM users") []
+    else
+      run conn ("CREATE TABLE users" ++
+                   " ( uuid INTEGER PRIMARY KEY ASC " ++
+                   " , name TEXT NOT NULL ) ") []
   commit conn
   disconnect conn
   return ()
